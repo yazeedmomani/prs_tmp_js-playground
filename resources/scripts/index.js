2febd7f43b6e7504b6f75bd2f008719c15ba95ce
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-21 14:34:04 +0300

    create template

---
+++ resources/scripts/index.js
@@ -0,0 +1,9 @@
+// ******************************
+// INCLUDED IN TEMPLATE
+// ******************************
+//Generates Random Number (Min and Max are included)
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + 1) + min - 1;
+
+// ******************************
+// NOT INCLUDED IN TEMPLATE
+// ******************************
